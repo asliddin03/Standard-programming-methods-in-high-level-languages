c35ed0a7faf9a09af7d3c7414428981c77a01981
asliddin03 <boltaev.ak@phystech.edu>
2024-06-03 16:16:37 +0300

    first solution before verification

--- complex/complex.cpp
+++ complex/complex.cpp
@@ -3,5 +3,59 @@
 Complex::Complex(double real_, double imag_) : real_(real_), imag_(imag_) {}
 
 Complex &Complex::operator+=(const Complex &complex) {
-  return
+  real_ += complex.real_;
+  imag_ += complex.imag_;
+}
+
+Complex Complex::operator+(const Complex &complex) const {
+  Complex result{ *this };
+  result += complex;
+  return result;
+}
+
+Complex &Complex::operator-=(const Complex &complex) {
+  real_ -= complex.real_;
+  imag_ -= complex.imag_;
+}
+
+Complex Complex::operator-(const Complex &complex) const {
+  Complex result{ *this };
+  result -= complex;
+  return result;
+}
+
+Complex &Complex::operator*=(const Complex &complex) {
+  const double currReal{ real_ * complex.real_ - imag_ * complex.imag_ };
+  const double currImag{ imag_ * complex.real_ + real_ * complex.imag_ };
+  real_ = currReal;
+  imag_ = currImag;
+  return *this;
+}
+
+Complex Complex::operator*(const Complex &complex) const {
+  Complex result{ *this };
+  result *= complex;
+  return result;
+}
+
+Complex &Complex::operator/=(const Complex &complex) {
+  const double currReal{ (real_ * complex.real_ + imag_ * complex.imag_) / (complex.real_ * complex.real_ + complex.imag_ * complex.imag_) };
+  const double currImag{ (imag_ * complex.real_ - real_ * complex.imag_) / (complex.real_ * complex.real_ + complex.imag_ * complex.imag_) };
+  real_ = currReal;
+  imag_ = currImag;
+  return *this;
+}
+
+Complex Complex::operator/(const Complex &complex) const {
+  Complex result{ *this };
+  result /= complex;
+  return result;
+}
+
+double Complex::abs() {
+  return std::sqrt(real_ * real_ + imag_ * imag_);
+}
+
+double Complex::argument() {
+  return atan2(imag_, real_);
 }
--- complex/complex.hpp
+++ complex/complex.hpp
@@ -1,12 +1,34 @@
 #pragma once
 
+#include <cmath>
 #include <iostream>
 
 class Complex {
  public:
-  explicit Complex(double real_, double imag_);
+  explicit Complex(double real_ = 0.0, double imag_ = 0.0);
+
+  // Addition operators.
   Complex& operator+=(const Complex& complex);
+  Complex operator+(const Complex& complex) const;
+
+  // Substraction operators.
+  Complex& operator-=(const Complex& complex);
+  Complex operator-(const Complex& complex) const;
+
+  // Multiplication operators.
+  Complex& operator*=(const Complex& complex);
+  Complex operator*(const Complex& complex) const;
+
+  // Division operators.
+  Complex& operator/=(const Complex& complex);
+  Complex operator/(const Complex& complex) const;
+
+  // Trigonometric.
+  double abs();
+
+  double argument();
+
  private:
-  double real_ = 0.0;
-  double imag_ = 0.0;
+  double real_;
+  double imag_;
 };
